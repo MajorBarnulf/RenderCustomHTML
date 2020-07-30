b7775ac329e80079ccd3485edab69d70e515678f
MajorBarnulf <matthieu@imagevo.fr>
2020-07-31 00:17:40 +0200

    Update RenderCustomHTML.plugin.js

--- RenderCustomHTML.plugin.js
+++ RenderCustomHTML.plugin.js
@@ -33,31 +33,31 @@ module.exports = class RenderCustomHTML {
         ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), "https://raw.githubusercontent.com/MajorBarnulf/RenderCustomHTML/master/RenderCustomHTML.plugin.js");
 		console.log("%c[MB][Render Custom HTML] %cstarting", 'color:red', 'color: inherit');
 
-
+		window.MB = {};
 		// a list of the tags that allow execution of scripts
-		window.dangerousTagList = ["Event", "abort", "afterprint", "animationend", "animationiteration", "animationstart", "beforeprint", "beforeunload", "canplay", "canplaythrough", "contextmenu", "copy", "cut", "dblclick", "drag", "dragend", "dragenter", "dragleave", "dragover", "dragstart", "drop", "durationchange", "ended", "error", "focusin", "focusout", "fullscreenchange", "fullscreenerror", "hashchange", "input", "invalid", "keydown", "keypress", "keyup", "load", "loadeddata", "loadedmetadata", "loadstart", "message", "mousedown", "mouseenter", "mouseleave", "mousemove", "mouseover", "mouseout", "mouseup", "mousewheel", "offline", "online", "open", "pagehide", "pageshow", "paste", "pause", "play", "playing", "popstate", "progress", "ratechange", "resize", "reset", "search", "seeked", "seeking", "select", "show", "stalled", "storage", "submit", "suspend", "timeupdate", "toggle", "touchcancel", "touchend", "touchmove", "touchstart", "transitionend", "unload", "volumechange", "waiting", "wheel"]
+		window.MB.dangerousTagList = ["Event", "abort", "afterprint", "animationend", "animationiteration", "animationstart", "beforeprint", "beforeunload", "canplay", "canplaythrough", "contextmenu", "copy", "cut", "dblclick", "drag", "dragend", "dragenter", "dragleave", "dragover", "dragstart", "drop", "durationchange", "ended", "error", "focusin", "focusout", "fullscreenchange", "fullscreenerror", "hashchange", "input", "invalid", "keydown", "keypress", "keyup", "load", "loadeddata", "loadedmetadata", "loadstart", "message", "mousedown", "mouseenter", "mouseleave", "mousemove", "mouseover", "mouseout", "mouseup", "mousewheel", "offline", "online", "open", "pagehide", "pageshow", "paste", "pause", "play", "playing", "popstate", "progress", "ratechange", "resize", "reset", "search", "seeked", "seeking", "select", "show", "stalled", "storage", "submit", "suspend", "timeupdate", "toggle", "touchcancel", "touchend", "touchmove", "touchstart", "transitionend", "unload", "volumechange", "waiting", "wheel"]
 		//TODO: treat "blur", "click", "focus", "scroll"
 
-		window.stringUnsanitizer = (str) => {
+		window.MB.stringUnsanitizer = (str) => {
 			return str = str.split("&amp;").join("&").split("&lt;").join("<").split("&gt;").join(">").split("&quot;").join('"').split("&#039;").join("'");
 		}
 
-		window.stringToHtml = (str) => {
+		window.MB.stringToHtml = (str) => {
 			var parser = new DOMParser();
-			str = window.stringUnsanitizer(str);
+			str = window.MB.stringUnsanitizer(str);
 			//console.log(str);
 			var doc = parser.parseFromString(str, 'text/html');
 			return doc.body;
 		}
 
-		window.securizeDOMElement = (inputElement) => {
+		window.MB.securizeDOMElement = (inputElement) => {
 			var allElements = inputElement.getElementsByTagName("*");
 			var unsafeElements = inputElement.querySelectorAll("script, link");
 			
 			for (var i = 0; i < allElements.length; i++) {
 				let element = allElements[i];
-				for (let j = 0; j < window.dangerousTagList.length; j++) {
-					var property = window.dangerousTagList[j];
+				for (let j = 0; j < window.MB.dangerousTagList.length; j++) {
+					var property = window.MB.dangerousTagList[j];
 					if (element[property] && element[property] != null) {
 						console.log("found unsafe tag: "+ property)
 						unsafeElements.push(element);
@@ -71,6 +71,16 @@ module.exports = class RenderCustomHTML {
 			}
 			var outputElement = inputElement;
 			return outputElement;
+		}
+
+		window.MB.messageFormatter = (message) => {
+			message += "\n test"
+			return message;
+		}
+
+		window.MB.customMessageSender = (message) => {
+			message = window.MB.messageFormatter(message);
+			ZLibrary.DiscordAPI.currentChannel.sendMessage(message);
 		}
 	} // Called when the plugin is activated (including after reloads)
 
@@ -88,9 +98,9 @@ module.exports = class RenderCustomHTML {
 					element.classList.add("mb-checked");
 					if (element.innerHTML.includes("/RCH")){
 						var htmlData = element.innerHTML.replace("/RCH", "");
-						var newDocument = window.stringToHtml(htmlData);
+						var newDocument = window.MB.stringToHtml(htmlData);
 						element.innerHTML = "";
-						element.appendChild(window.securizeDOMElement(newDocument));
+						element.appendChild(window.MB.securizeDOMElement(newDocument));
 					}
 				}
 			}
