de75d946c83371580ae12a702220e4529b881a37
MajorBarnulf <matthieu@imagevo.fr>
2020-07-30 18:58:49 +0200

    Update RenderCustomHTML.plugin.js

--- RenderCustomHTML.plugin.js
+++ RenderCustomHTML.plugin.js
@@ -17,7 +17,7 @@ module.exports = class RenderCustomHTML {
 		return "Will render the HTML contained in messages using \"/RCH\" ";
 	} // Description to show on the plugins page
 	getVersion() {
-		return "0.1.5";
+		return "0.1.6";
 	} // Current version. I recommend following semantic versioning <http://semver.org/> (e.g. 0.0.1)
 	getAuthor() {
 		return "MajorBarnulf";
